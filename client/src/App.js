d5398d245eaf6f559e2f70cef3a0386e72798a9c
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-12-07 20:00:03 +0900

    update some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import Settings from "./components/settings.component";
+import CustomersList from "./components/customers-list.component";
+import EditCustomers from "./components/edit-customers.component";
+import CreateCustomers from "./components/create-customers.component";
+import Schedule from "./components/schedule.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <div className="container">
+          <Route path="/admin" exact component={CustomersList} />
+          <Route path="/admin/settings" exact component={Settings} />
+          <Route path="/admin/edit/:id" exact component={EditCustomers} />
+          <Route path="/admin/create" exact component={CreateCustomers} />
+          <Route path="/admin/schedule" exact component={Schedule} />
+        </div>
+    </Router>
   );
 }
 
