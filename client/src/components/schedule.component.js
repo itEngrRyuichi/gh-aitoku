791688a724dae6fb034150e9374633ca57b8baf3
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-12-08 13:40:52 +0900

    localhost:5000 to aitoku-server.herokuapp.com

--- client/src/components/schedule.component.js
+++ client/src/components/schedule.component.js
@@ -35,7 +35,7 @@ export default class Schedule extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:5000/schedule/')
+        axios.get('http://aitoku-server.herokuapp.com/schedule/')
         .then(response => {
             this.setState({ events: 
                 response.data.map(currentcustomer => {
@@ -61,7 +61,7 @@ export default class Schedule extends Component {
     }
 
     openModal(id) {
-        axios.get('http://localhost:5000/customers/'+id)
+        axios.get('http://aitoku-server.herokuapp.com/customers/'+id)
         .then(response => {
             this.setState({
                 isOpen: true,
