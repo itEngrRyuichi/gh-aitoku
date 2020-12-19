dfc5bf08ef292aeca3a099d07fc6991ebeda51aa
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-12-20 00:23:51 +0900

    third floor

--- client/src/components/floorplan.component.js
+++ client/src/components/floorplan.component.js
@@ -17,30 +17,170 @@ export default class FloorMap extends Component {
     thirdFloor() {
         const canvas = this.refs.third;
         const ctx = canvas.getContext("2d");
-        canvas.width = window.innerWidth / 2;
-        canvas.height = window.innerHeight / 2;
-        // fillText ()
-        ctx.font = '25px Arial';
-        ctx.fillStyle = 'purple';
-        ctx.fillText('Hello World', 10, 50);
-    }
 
-    forthFloor() {
-        const canvas = this.refs.forth;
-        const ctx = canvas.getContext("2d");
+        const boldLine = 3;
+        const Line = 1;
+        const tinyLine = 0.5;
 
-        const boldLine = 1;
-        const Line = 0.5;
-
-        canvas.width = window.innerWidth / 2;
-        canvas.height = window.innerHeight / 2;
+        canvas.width = window.innerWidth / 3 * 2;
+        canvas.height = window.innerHeight;
         
         ctx.strokeStyle = 'black';
 
         ctx.shadowColor = 'gray';
         ctx.shadowOffsetX = 10;
         ctx.shadowOffsetY = 10;
-        ctx.shadowBlur = 10;
+        ctx.shadowBlur = 20;
+
+        // 301
+        ctx.beginPath();
+       
+        ctx.moveTo(50, 50);
+        ctx.lineTo(230, 50);
+        ctx.lineTo(230, 230);
+        ctx.lineTo(50, 230);
+        ctx.closePath();
+        ctx.lineWidth = boldLine;
+        ctx.stroke();
+
+        ctx.font = '25px Arial';
+        ctx.fillText('301', 70, 100);
+
+        // 302
+        ctx.beginPath();
+        ctx.moveTo(230, 50);
+        ctx.lineTo(380, 50);
+        ctx.lineTo(380, 230);
+        ctx.lineTo(230, 230);
+        ctx.closePath();
+        ctx.lineWidth = boldLine;
+        ctx.stroke();
+
+        ctx.font = '25px Arial';
+        ctx.fillText('302', 250, 100);
+
+        // 303
+        ctx.beginPath();
+        ctx.moveTo(380, 50);
+        ctx.lineTo(560, 50);
+        ctx.lineTo(560, 80);
+        ctx.lineTo(590, 90);
+        ctx.lineTo(590, 200);
+        ctx.lineTo(560, 200);
+        ctx.lineTo(560, 230);
+        ctx.lineTo(380, 230);
+        ctx.closePath();
+        ctx.lineWidth = boldLine;
+        ctx.stroke();
+
+        ctx.font = '25px Arial';
+        ctx.fillText('303', 400, 100);
+
+        // 305
+        ctx.beginPath();
+        ctx.moveTo(590, 90);
+        ctx.lineTo(740, 140);
+        ctx.lineTo(740, 200);
+        ctx.lineTo(620, 200);
+        ctx.lineTo(620, 230);
+        ctx.lineTo(560, 230);
+        ctx.lineTo(560, 200);
+        ctx.lineTo(590, 200);
+        ctx.closePath();
+        ctx.lineWidth = boldLine;
+        ctx.stroke();
+
+        ctx.font = '25px Arial';
+        ctx.fillText('305', 610, 140);
+
+        // wc
+        ctx.beginPath();
+        ctx.moveTo(620, 200);
+        ctx.lineTo(740, 200);
+        ctx.lineTo(740, 350);
+        ctx.lineTo(590, 350);
+        ctx.lineTo(590, 290);
+        ctx.lineTo(680, 290);
+        ctx.lineTo(680, 260);
+        ctx.lineTo(620, 260);
+        ctx.closePath();
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        // washbassin
+        ctx.beginPath();
+        ctx.moveTo(425, 305);
+        ctx.lineTo(590, 305);
+        ctx.lineTo(590, 350);
+        ctx.lineTo(425, 350);
+        ctx.closePath();
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        // steps
+        ctx.beginPath();
+        ctx.moveTo(50, 230);
+        ctx.lineTo(200, 230);
+        ctx.lineTo(200, 350);
+        ctx.lineTo(50, 350);
+        ctx.closePath();
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(200, 290);
+        ctx.lineTo(110, 290);
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(185, 230);
+        ctx.lineTo(185, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(170, 230);
+        ctx.lineTo(170, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(155, 230);
+        ctx.lineTo(155, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(140, 230);
+        ctx.lineTo(140, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(125, 230);
+        ctx.lineTo(125, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+    }
+
+    forthFloor() {
+        const canvas = this.refs.forth;
+        const ctx = canvas.getContext("2d");
+
+        const boldLine = 3;
+        const Line = 1;
+        const tinyLine = 0.5;
+
+        canvas.width = window.innerWidth / 3 * 2;
+        canvas.height = window.innerHeight / 3 * 2;
+        
+        ctx.strokeStyle = 'black';
+
+        ctx.shadowColor = 'gray';
+        ctx.shadowOffsetX = 10;
+        ctx.shadowOffsetY = 10;
+        ctx.shadowBlur = 20;
 
         // 401
         ctx.beginPath();
@@ -117,12 +257,79 @@ export default class FloorMap extends Component {
         ctx.lineWidth = Line;
         ctx.stroke();
 
-        ctx.font = '15px Arial';
-        ctx.fillText('WC', 640, 250);
-
         // washbassin
-        // corridor
+        ctx.beginPath();
+        ctx.moveTo(425, 305);
+        ctx.lineTo(590, 305);
+        ctx.lineTo(590, 350);
+        ctx.lineTo(425, 350);
+        ctx.closePath();
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
         // steps
+        ctx.beginPath();
+        ctx.moveTo(50, 230);
+        ctx.lineTo(200, 230);
+        ctx.lineTo(200, 350);
+        ctx.lineTo(50, 350);
+        ctx.closePath();
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(200, 290);
+        ctx.lineTo(110, 290);
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(185, 230);
+        ctx.lineTo(185, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(170, 230);
+        ctx.lineTo(170, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(155, 230);
+        ctx.lineTo(155, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(140, 230);
+        ctx.lineTo(140, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(125, 230);
+        ctx.lineTo(125, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(110, 230);
+        ctx.lineTo(110, 350);
+        ctx.lineWidth = tinyLine;
+        ctx.stroke();
+
+        // corridor
+        ctx.beginPath();
+        ctx.moveTo(50, 50);
+        ctx.lineTo(560, 50);
+        ctx.lineTo(560, 80);
+        ctx.lineTo(740, 140);
+        ctx.lineTo(740, 350);
+        ctx.lineTo(50, 350);
+        ctx.closePath();
+        ctx.lineWidth = 3;
+        ctx.stroke();
 
     }
 
@@ -136,7 +343,7 @@ export default class FloorMap extends Component {
                 <br />
                 <label>4階 平面図</label>
                 <br />
-                <canvas ref='forth'/>
+                <canvas className="py-5" ref='forth'/>
             </>
         )
     }
