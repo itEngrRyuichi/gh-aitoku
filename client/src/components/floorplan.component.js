4b3fcb083f4d95f290be4e58275d997cca47807d
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-12-19 23:36:50 +0900

    added wv

--- client/src/components/floorplan.component.js
+++ client/src/components/floorplan.component.js
@@ -17,8 +17,8 @@ export default class FloorMap extends Component {
     thirdFloor() {
         const canvas = this.refs.third;
         const ctx = canvas.getContext("2d");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth / 2;
+        canvas.height = window.innerHeight / 2;
         // fillText ()
         ctx.font = '25px Arial';
         ctx.fillStyle = 'purple';
@@ -29,14 +29,23 @@ export default class FloorMap extends Component {
         const canvas = this.refs.forth;
         const ctx = canvas.getContext("2d");
 
-        const boldLine = 1.5;
+        const boldLine = 1;
+        const Line = 0.5;
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth / 2;
+        canvas.height = window.innerHeight / 2;
+        
+        ctx.strokeStyle = 'black';
+
+        ctx.shadowColor = 'gray';
+        ctx.shadowOffsetX = 10;
+        ctx.shadowOffsetY = 10;
+        ctx.shadowBlur = 10;
 
         // 401
         ctx.beginPath();
-        ctx.moveTo(50, 50);
+       
+        ctx.moveTo(50, 50);
         ctx.lineTo(230, 50);
         ctx.lineTo(230, 230);
         ctx.lineTo(50, 230);
@@ -93,6 +102,28 @@ export default class FloorMap extends Component {
 
         ctx.font = '25px Arial';
         ctx.fillText('405', 610, 140);
+
+        // wc
+        ctx.beginPath();
+        ctx.moveTo(620, 200);
+        ctx.lineTo(740, 200);
+        ctx.lineTo(740, 350);
+        ctx.lineTo(590, 350);
+        ctx.lineTo(590, 290);
+        ctx.lineTo(680, 290);
+        ctx.lineTo(680, 260);
+        ctx.lineTo(620, 260);
+        ctx.closePath();
+        ctx.lineWidth = Line;
+        ctx.stroke();
+
+        ctx.font = '15px Arial';
+        ctx.fillText('WC', 640, 250);
+
+        // washbassin
+        // corridor
+        // steps
+
     }
 
     render() {
@@ -100,9 +131,11 @@ export default class FloorMap extends Component {
         return(
             <>
                 <label>3階 平面図</label>
+                <br />
                 <canvas ref='third'/>
                 <br />
                 <label>4階 平面図</label>
+                <br />
                 <canvas ref='forth'/>
             </>
         )
