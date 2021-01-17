7a712b62be08d80068a913ac602040db5e964dcf
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-01-17 22:23:27 +0900

    remove some

--- client/src/components/floorMap.component.jsx
+++ client/src/components/floorMap.component.jsx
@@ -11,35 +11,56 @@ export default class FloorMap extends Component {
         this.thirdFloor();
     }
 
+    commonBoxStyles(canvas, ctx) {
+        canvas.width = 120;
+        canvas.height = 120;
+        canvas.style.width  = '120px';
+        canvas.style.height = '120px';
+        ctx.fillStyle = 'black';
+        ctx.font = '18px Arial';
+    }
+
+    commonRectStyles(canvas, ctx) {
+        canvas.width = 240;
+        canvas.height = 120;
+        canvas.style.width  = '240px';
+        canvas.style.height = '120px';
+        ctx.fillStyle = 'black';
+        ctx.font = '18px Arial';
+    }
+
     thirdFloor() {
         // Room 301
         const canvasRoom301 = this.refs.room301;
         const ctxRoom301 = canvasRoom301.getContext("2d");
+        this.commonBoxStyles(canvasRoom301, ctxRoom301);
         ctxRoom301.fillText('301', 20, 40);
         ctxRoom301.fillText('4人部屋', 20, 60);
 
         // Room 302
         const canvasRoom302 = this.refs.room302;
         const ctxRoom302 = canvasRoom302.getContext("2d");
+        this.commonBoxStyles(canvasRoom302, ctxRoom302);
         ctxRoom302.fillText('302', 20, 40);
         ctxRoom302.fillText('4人部屋', 20, 60);
 
         // Room 303
         const canvasRoom303 = this.refs.room303;
         const ctxRoom303 = canvasRoom303.getContext("2d");
+        this.commonBoxStyles(canvasRoom303, ctxRoom303);
         ctxRoom303.fillText('303', 20, 40);
         ctxRoom303.fillText('4人部屋', 20, 60);
 
         // Room 305
         const canvasRoom305 = this.refs.room305;
         const ctxRoom305 = canvasRoom305.getContext("2d");
+        this.commonBoxStyles(canvasRoom305, ctxRoom305);
         ctxRoom305.fillText('305', 15, 70);
         ctxRoom305.fillText('2人部屋', 15, 90);
 
         // Third Floor Corridor
         const canvasThirdFloorCorridor = this.refs.thirdFloorCorridor;
         const ctxThirdFloorCorridor = canvasThirdFloorCorridor.getContext("2d");
-        
         canvasThirdFloorCorridor.width = 480;
         canvasThirdFloorCorridor.height = 120;
         canvasThirdFloorCorridor.style.width  = '480px';
