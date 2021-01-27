d4ce0c2471be2fab9920b80f9cebb97277cfb0a9
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-01-28 03:47:39 +0900

    added some meta

--- client/src/Head.js
+++ client/src/Head.js
@@ -14,6 +14,7 @@ class Head extends React.Component {
                     <meta name="google-site-verification" content="ym6_1XKOzOZ7W4q6QZ6n4GKT3iTM-S0yPcaF2v5zc0w" />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="description" content="「ただいま」って言いたくなるあたたかい宿泊施設です。野沢温泉村、ゲストハウス愛徳で宿泊しませんか？ ゲレンデまで徒歩1分。スキーシーズンもグリーンシーズンも楽しもう！スキー、スノーボードは勿論、夏は涼しく自然の中でリモートワークも最適！" />
+                    <meta name="copyright" content="ゲストハウス愛徳"></meta>
                     {/* OGP Tag */}
                     <meta property="og:title" content="ゲストハウス愛徳" />
                     <meta property="og:type" content="website" />
