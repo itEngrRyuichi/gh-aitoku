56de05e253112d3775bb63bc17bb5a2e33955316
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-02-05 22:52:00 +0900

    reset reservation

--- client/src/components/clientView/reservation.component.jsx
+++ client/src/components/clientView/reservation.component.jsx
@@ -13,33 +13,6 @@ export default class Reservation extends Component {
     }
     
     componentDidMount() {
-        this.animated();
-    }
-
-    animated() {
-        const configReserveWindow = {
-        origin: 'right',
-        duration: 500,
-        delay: 0,
-        distance: '1000px',
-        scale: 1,
-        easing: 'ease-in-out',
-        }
-
-        sr.reveal(this.refs.reserveWindow, configReserveWindow);
-    }
-
-    closed() {
-        const configCloseWindows = {
-            origin: 'left',
-            duration: 500,
-            delay: 0,
-            distance: '1000px',
-            scale: 1,
-            easing: 'ease-out',
-        }
-
-        sr.reveal(this.refs.reserveWindow, configCloseWindows);
     }
 
     render() {
@@ -50,7 +23,6 @@ export default class Reservation extends Component {
                         animated={false}
                         page={'reservation'} />
                     <div className="reservation" ref='reserveWindow'>
-                        <p onClick={ this.closed() } className="returnHome"><i className="fas fa-arrow-right"></i></p>
                     </div>
                 </div>
                 <Footer />
