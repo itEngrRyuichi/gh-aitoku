74dc55df565a9bd34fd894c144e5f3bf43a30d81
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-02-28 18:43:25 +0900

    this is for Head

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -1,8 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-/* import Ready from './ready.component'; */
 import MainPage from './clientView/mainpage.component';
-import ogpics from "../pics/ogpics.jpg";
 
 export default class Home extends Component {
     constructor(props) {
@@ -22,8 +20,8 @@ export default class Home extends Component {
                     {/* OGP Tag */}
                     <meta property="og:title" content="ゲストハウス愛徳" />
                     <meta property="og:type" content="website" />
-                    <meta property="og:url" content="https://guesthouseaitoku.herokuapp.com/" />
-                    <meta property="og:image" content={ogpics} />
+                    <meta property="og:url" content="https://ghaitoku.herokuapp.com/" />
+                    <meta property="og:image" content="https://ghaitoku.herokuapp.com/ogpics.jpg" />
                     <meta property="og:site_name" content="ゲストハウス愛徳" />
                     <meta property="og:description" content="「ただいま」って言いたくなるあたたかい宿泊施設。野沢温泉村、ゲストハウス愛徳で宿泊しませんか？ ゲレンデまで徒歩1分。スキースノボは勿論、夏は涼しく自然の中でリモートワークも最適！" />
                 </Helmet>
